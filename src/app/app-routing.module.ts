5220b5864c26d1ddb78196d3e46db3116451acc6
rahulkantubhukta <rkantubhukta@gmail.com>
2022-10-26 02:13:20 +0530

    university component changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { SearchBarComponent } from './university/search-bar/search-bar.component';
+import { UniversityComponent } from './university/university.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'university',
+    component: UniversityComponent,
+    children:[
+      {
+        path:'search',
+        component: SearchBarComponent
+      }
+    ]
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UniversityComponent } from './university/university.component';
+import { StudentComponent } from './student/student.component';
+import { FilterPipe } from './shared/filter.pipe';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { SearchBarComponent } from './university/search-bar/search-bar.component';
+import { FileUploadComponent } from './university/file-upload/file-upload.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    UniversityComponent,
+    StudentComponent,
+    FilterPipe,
+    SearchBarComponent,
+    FileUploadComponent,
+    ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
