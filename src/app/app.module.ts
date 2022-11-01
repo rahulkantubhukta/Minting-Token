851eee542e383040cf66f934e08a59646872a0a0
rahulkantubhukta <rkantubhukta@gmail.com>
2022-11-02 01:36:42 +0530

    added loading spinner onupload and no file selected error msg on click of upload option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
-
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { SearchBarComponent } from './university/search-bar/search-bar.component';
 import { FileUploadComponent } from './university/file-upload/file-upload.component';
@@ -40,6 +40,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatInputModule,
     MatListModule,
+    MatProgressSpinnerModule,
     BrowserAnimationsModule,
     HttpClientModule,
   ],
